ad8bfefa2d2354a37b877c2957c53208c928f3b3
Dan Byers <dcbyers@mmm.com>
2018-12-04 23:18:52 -0600

    Defined chapter.

--- src/edu/bobjones/chapters/Chapter.java
+++ src/edu/bobjones/chapters/Chapter.java
@@ -1,6 +1,20 @@
 package edu.bobjones.chapters;
 
-public interface Chapter {
-    Chapter nextChapter();
-    void tellStory();
+import edu.bobjones.Setting;
+
+public abstract class Chapter {
+	private final Chapter nextChapter;
+	private final Setting setting;
+	
+	public Chapter(Setting setting, Chapter nextChapter) {
+		this.setting = setting;
+		this.nextChapter = nextChapter;
+	}
+    public Chapter nextChapter() {
+    	return nextChapter;
+    }
+    
+    public abstract String getTitle();
+
+    public abstract String getStory();
 }
